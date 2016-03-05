cb26f16ef0d69e749337a1301709f625fec4a1ee
ilya-belkin <ilya-belkin@list.ru>
2016-03-05 14:06:06 +0300

    finished exercise 1

---
+++ ITVDN/ASP.NET/Lesson 1/Ex_1/Ex_1/Autorization.aspx.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Ex_1
+{
+    public partial class Autorization : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            if (TextBox1.Text == "admin" && TextBox2.Text == "qwerty")
+                Label1.Text = "Succesful autorization";
+            else
+                Label1.Text = "Error. Repeat, please!";
+        }
+    }
+}
